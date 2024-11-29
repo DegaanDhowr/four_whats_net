3cf0a1cba5e6059c0e48b78528bfd1c166bfc06e
mohamed Abdirahman <mohacali452@gmail.com>
2024-11-30 00:23:34 +0300

    Added sms config and messege list

---
+++ four_whats_net/four_whats_net/doctype/hormuud_sms_configuration/hormuud_sms_configuration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, hts-qatar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Hormuud SMS Configuration', {
+	// refresh: function(frm) {
+
+	// }
+});
